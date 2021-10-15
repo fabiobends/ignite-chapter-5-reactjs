5fdc76f2127a0c647ac75b2e903144df99f16b06
Fábio Mendes <fabio.mendes@profusion.mobi>
2021-10-15 20:44:11 -0300

    feat: formatting data example

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -2,19 +2,15 @@ import { useMemo } from "react";
 import { ProductItem } from "./ProductItem";
 
 type SearchResultsProps = {
-  results: Array<{ id: number; price: number; title: string }>;
+  results: Array<{ id: number; price: number; priceFormatted: string; title: string }>;
   addToWishList: (id: number) => void;
+  totalPrice: number;
 };
 
-export function SearchResults({ results, addToWishList }: SearchResultsProps) {
-  const totalPrice = useMemo(() => {
-    return results.reduce((total, result) => {
-      return total + result.price;
-    }, 0);
-  }, []);
-
+export function SearchResults({ results, totalPrice, addToWishList }: SearchResultsProps) {
   return (
     <div>
+      <h1>{totalPrice}</h1>
       {/* useMemo is worth it
         <Component totalPrice={totalPrice} />
         */}
