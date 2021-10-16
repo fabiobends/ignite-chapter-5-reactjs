d6a2c7c40d813eedd00900a6da2de39b66714746
Fábio Mendes <fabio.mendes@profusion.mobi>
2021-10-16 09:44:28 -0300

    feat: virtualization example

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,26 +1,41 @@
-import { useMemo } from "react";
+import { List, ListRowRenderer } from "react-virtualized";
 import { ProductItem } from "./ProductItem";
 
 type SearchResultsProps = {
-  results: Array<{ id: number; price: number; priceFormatted: string; title: string }>;
+  results: Array<{
+    id: number;
+    price: number;
+    priceFormatted: string;
+    title: string;
+  }>;
   addToWishList: (id: number) => void;
   totalPrice: number;
 };
 
-export function SearchResults({ results, totalPrice, addToWishList }: SearchResultsProps) {
+export function SearchResults({
+  results,
+  totalPrice,
+  addToWishList,
+}: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem product={results[index]} addToWishList={addToWishList} />
+      </div>
+    );
+  };
+
   return (
     <div>
       <h1>{totalPrice}</h1>
-      {/* useMemo is worth it
-        <Component totalPrice={totalPrice} />
-        */}
-      {results.map((result) => (
-        <ProductItem
-          key={result.id}
-          product={result}
-          addToWishList={addToWishList}
-        />
-      ))}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
